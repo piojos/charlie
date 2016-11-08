43a3c6c9971d20434affee54d3e2b7102100510d
raidho-mx <miranda@raidho.mx>
2016-11-07 18:00:42 -0600

    Editable Featured food block

--- inc/card-food.php
+++ inc/card-food.php
@@ -1,5 +1,28 @@
-<a class="slide dish">
-	<img src="http://placehold.it/380x300&text=BURGER"><br>
-	<span class="opts foodtruck"></span><span class="price">100</span><span class="opts delivery yes"></span>
-	<h3>Toreados Burger</h3>
+<?php
+
+	while (have_rows('video')) {
+		the_row();
+		$fallback = get_sub_field('fallback');
+	}
+	$options = get_field('options');
+	if(in_array('delivery', $options)) $canDeliver = ' yes';
+	if(in_array('foodtruck', $options)) $canFoodtruck = ' yes';
+	$price = get_field('prize');
+	$estTime = get_field('delivery_time');
+	$about = get_field('description');
+
+
+
+?><a href="<?php the_permalink(); ?>" class="slide dish"><?php
+
+
+if($fallback) { ?>
+	<img src="<?php echo $fallback['sizes']['large']; ?>" alt="<?php echo $fallback['alt']; ?>"><?php
+} ?><br>
+	<span class="opts foodtruck<?php echo $canFoodtruck; ?>"></span>
+	<?php if($price) {
+		echo '<span class="price">'. $price . '</span>';
+	} ?>
+	<span class="opts delivery<?php echo $canDeliver; ?>"></span>
+	<h3><?php the_title(); ?></h3>
 </a>
--- inc/slider-food.php
+++ inc/slider-food.php
@@ -1,10 +1,15 @@
-<div class="slider wrap">
+<?php
 
-	<?php get_template_part('inc/card', 'food'); ?>
-	<?php get_template_part('inc/card', 'food'); ?>
-	<?php get_template_part('inc/card', 'food'); ?>
-	<?php get_template_part('inc/card', 'food'); ?>
-	<?php get_template_part('inc/card', 'food'); ?>
-	<?php get_template_part('inc/card', 'food'); ?>
+	// when on home
+	$ftd_food = get_sub_field('selected_food');
 
-</div>
+
+	if( $ftd_food ): ?>
+<div class="slider wrap"><?php
+	foreach( $ftd_food as $post):
+		setup_postdata($post);
+	get_template_part('inc/card', 'food'); 
+	endforeach; ?>
+</div><?php
+	wp_reset_postdata();
+	endif; ?>
