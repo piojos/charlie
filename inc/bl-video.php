efa2a0f59692ea264874a92fc43fccf5cabafb3d
raidho-mx <miranda@raidho.mx>
2016-11-10 14:19:45 -0600

    Working Page blocks

---
+++ inc/bl-video.php
@@ -0,0 +1,5 @@
+<?php /*
+<div class="block video">
+	<!-- video -->
+</div>
+*/ ?>
--- inc/blocks-manager.php
+++ inc/blocks-manager.php
@@ -15,17 +15,28 @@ if( have_rows('blocks') ):
 		elseif( get_row_layout() == 'ratings' ):
 			get_template_part('inc/bl', 'ratings');
 
-		elseif( get_row_layout() == 'separator' ):
-			// get_template_part('inc/bl', '');
-			// $options = get_sub_field('options'); ?>
+		elseif( get_row_layout() == 'separator' ): ?>
 			<div class="block separator<?php echo blockClass(get_sub_field('options')); ?>">
 				<div class="wrap">
 					<hr>
 				</div>
 			</div><?php
-		// elseif( get_row_layout() == '' ):
-		// 	get_template_part('inc/bl', '');
-		//
+
+		elseif( get_row_layout() == 'video' ):
+			get_template_part('inc/bl', 'video');
+
+		elseif( get_row_layout() == 'about' ):
+			get_template_part('inc/bl', 'about');
+
+		elseif( get_row_layout() == 'faq' ):
+			get_template_part('inc/bl', 'faq');
+
+		elseif( get_row_layout() == 'news' ):
+			get_template_part('inc/bl', 'newsfeed');
+
+		elseif( get_row_layout() == 'contact' ):
+			get_template_part('inc/bl', 'contacto');
+
 		// elseif( get_row_layout() == '' ):
 		// 	get_template_part('inc/bl', '');
 
