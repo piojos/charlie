0d7e155a55834276d3662ac517611f84e527b1c0
raidho-mx <miranda@raidho.mx>
2016-11-16 17:28:06 -0600

    Added food size

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ if (function_exists('add_theme_support'))
 
 	// Add Thumbnail Theme Support
 	add_theme_support('post-thumbnails');
-	// add_image_size('large', 700, '', true); // Large Thumbnail
+	add_image_size('food', 570, 450, true); // FOOD Thumbnail
 	// add_image_size('medium', 250, '', true); // Medium Thumbnail
 	// add_image_size('small', 120, '', true); // Small Thumbnail
 	// add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
