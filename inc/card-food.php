88a68f7c07acaa02c25da5df2b8e8038f3433f16
raidho-mx <miranda@raidho.mx>
2016-11-23 11:24:53 -0600

    Added video on dishes

--- inc/card-food.php
+++ inc/card-food.php
@@ -2,6 +2,8 @@
 
 	while (have_rows('video')) {
 		the_row();
+		$mp4 = get_sub_field('mp4');
+		$webm = get_sub_field('webm');
 		$fallback = get_sub_field('fallback');
 	}
 	$options = get_field('options');
@@ -13,14 +15,23 @@
 
 
 
-?><a href="<?php the_permalink(); ?>" class="slide dish"><?php
+?><a href="<?php the_permalink(); ?>" class="slide dish<?php if($canDeliver) echo ' delivery'; if($canFoodtruck) echo ' foodtruck'; ?>"><?php
 
+if($mp4 OR $webm) : ?>
+	<video poster="<?php echo $fallback['sizes']['food']; ?>" playsinline autoplay muted loop><?php
+	if($webm) { ?>
+		<source src='<?php echo $webm[url]; ?>' type='video/webm'><?php
+	}
 
-if($fallback) { ?>
+	if($mp4) { ?>
+		<source src='<?php echo $mp4[url]; ?>' type='video/mp4'><?php
+	} ?>
+	</video><?php
+elseif($fallback) : ?>
 	<img src="<?php echo $fallback['sizes']['food']; ?>" alt="<?php echo $fallback['alt']; ?>"><?php
-} else { ?>
+else : ?>
 	<img src="http://placehold.it/380x300&text=MUCHO+RICO" alt="No image"><?php
-} ?><br>
+endif; ?><br>
 	<span class="opts foodtruck<?php echo $canFoodtruck; ?>"></span>
 	<?php if($price) {
 		echo '<span class="pricetag">'. $price . '</span>';
