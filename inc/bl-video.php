35ed8dce4a5fe78d0303f4ba71712811de04f3e3
raidho-mx <miranda@raidho.mx>
2016-12-08 12:30:24 -0600

    Better validation

--- inc/bl-video.php
+++ inc/bl-video.php
@@ -1,7 +1,4 @@
-<?php /*
-<div class="block video">
-	<!-- video -->
-</div> */
+<?php 
 
 if(have_rows('intro')) :
 	while(have_rows('intro')) { the_row();
@@ -15,25 +12,28 @@ else :
 	$webm = get_sub_field('webm');
 	$mp4 = get_sub_field('mp4');
 endif;
-$animatedLogo = get_field('logo_anim', 'options');
 
- ?>
+	$animatedLogo = get_field('logo_anim', 'options');
 
+	if($mp4 OR $webm OR $fallback) : ?>
 
-<div class="block video<?php echo $introClass; ?>"><?php
-	if( !empty($animatedLogo) ): ?>
-	<div class="message">
-		<img id="sticky" src="<?php echo $animatedLogo['url']; ?>" alt="<?php echo $animatedLogo['alt']; ?>" class="random" data-aos="huge-out" data-aos-anchor-placement="top-top" />
+	<div class="block video<?php echo $introClass; ?>"><?php
+		if( !empty($animatedLogo) ): ?>
+		<div class="message">
+			<img id="sticky" src="<?php echo $animatedLogo['url']; ?>" alt="<?php echo $animatedLogo['alt']; ?>" class="random" data-aos="huge-out" data-aos-anchor-placement="top-top" />
+		</div><?php
+		endif; ?>
+		<video poster='<?php echo $fallback[url]; ?>' playsinline autoplay muted loop ><?php
+		if($webm) { ?>
+			<source src='<?php echo $webm[url]; ?>' type='video/webm'><?php
+		}
+
+		if($mp4) { ?>
+			<source src='<?php echo $mp4[url]; ?>' type='video/mp4'><?php
+		} ?>
+		</video>
+
 	</div><?php
-	endif; ?>
-	<video poster='<?php echo $fallback[url]; ?>' playsinline autoplay muted loop ><?php
-	if($webm) { ?>
-		<source src='<?php echo $webm[url]; ?>' type='video/webm'><?php
-	}
 
-	if($mp4) { ?>
-		<source src='<?php echo $mp4[url]; ?>' type='video/mp4'><?php
-	} ?>
-	</video>
-
-</div>
+	endif;
+?>
